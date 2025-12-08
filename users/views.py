f8bd4e67753ace4384f208c4011874e83bedb717
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-12-08 15:43:02 -0300

    feat: adiciona django rest, rota para listagem de clientes e serialização para apenas leitura

--- users/views.py
+++ users/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets, permissions
+from .models import PerfilCliente
+from .serializers import PerfilClienteSerializer
 
-# Create your views here.
-def home(request):
-    return HttpResponse("<h1>Teste</h1>")
+# classe para visualizar os perfis dos clientes
+class PerfilClienteViewSet(viewsets.ReadOnlyModelViewSet):
+    # pega os perfis de clientes
+    queryset = PerfilCliente.objects.select_related('user').all()
+    # usa o serializador de perfil de cliente
+    serializer_class = PerfilClienteSerializer
+    permission_classes = [permissions.IsAuthenticated]
