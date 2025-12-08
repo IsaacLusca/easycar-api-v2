c42ce708fd329ebece534d1c45b0a5e2bb260ac5
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-12-08 16:11:12 -0300

    feat: permite que user staff visualize todos os clientes e restringe quem cliente consegue visualizar

--- users/views.py
+++ users/views.py
@@ -6,8 +6,16 @@ from .serializers import PerfilClienteSerializer
 
 # classe para visualizar os perfis dos clientes
 class PerfilClienteViewSet(viewsets.ReadOnlyModelViewSet):
-    # pega os perfis de clientes
-    queryset = PerfilCliente.objects.select_related('user').all()
     # usa o serializador de perfil de cliente
     serializer_class = PerfilClienteSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # pega o usuário logado
+        user = self.request.user
+
+        # se for staf, retorna todos os perfis
+        if user.is_staff:
+            return PerfilCliente.objects.select_related('user').all()
+        
+        return PerfilCliente.objects.select_related('user').filter(user=user)
