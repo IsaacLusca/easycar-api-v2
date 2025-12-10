80c466f110ef5989b8c37992a049b1b719e9a6f9
Menzy <juhbeall@gmail.com>
2025-12-09 22:46:00 -0300

    serializer

---
+++ rentals/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Aluguel
+
+class AluguelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluguel
+        fields = ['id', 'perfil_cliente', 'carro', 'funcionario', 'data_inicio', 'data_fim', 'valor_total','status']
