8716918231815f37ce8771fa164aef0af6cffb00
IsaacLusca <isaaclucas.bezerra@gmail.com>
2026-07-15 20:58:44 -0300

    fix: configurar SQLite como padrao para dev local e PostgreSQL no Docker

--- backend/easycar/settings.py
+++ backend/easycar/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'easycar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'easycar_db'),
-        'USER': os.environ.get('DB_USER', 'easycar_user'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'easycar_password'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
 
